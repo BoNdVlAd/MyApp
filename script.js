2a863e3cb6b014608afa7f7065a87e26d8f61606
Bond Vlad <wlad23072003@gmail.com>
2024-04-16 19:35:32 +0300

    added_js

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToBuy() {
+  const el = document.getElementById('buy');
+  el.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToGoods() {
+  const el = document.getElementById('goods');
+  el.scrollIntoView({ behavior: 'smooth' });
+}
